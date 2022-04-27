a6b8d10223b5e248e9473ea6bd3f29f76a3164e3
Angelo Rimoldi <angeloerimoldi@gmail.com>
2022-04-27 18:45:35 +0200

    Updates

---
+++ precache-manifest.85aff26dc38c08fb87c15727ead1e6be.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "071e7d027e5a630a0e442384feb590f7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fb4ab1beebd6d47667fe",
+    "url": "/static/css/main.00d54f48.chunk.css"
+  },
+  {
+    "revision": "aca6a2fa0a943172648a",
+    "url": "/static/js/2.638527dd.chunk.js"
+  },
+  {
+    "revision": "569e878394263cd68864b6526c81f10b",
+    "url": "/static/js/2.638527dd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fb4ab1beebd6d47667fe",
+    "url": "/static/js/main.10ef58c9.chunk.js"
+  },
+  {
+    "revision": "41d0f2c29c14695d2f6c",
+    "url": "/static/js/runtime-main.e968c2a6.js"
+  }
+]);
